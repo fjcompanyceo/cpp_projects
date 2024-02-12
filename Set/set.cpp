d856acae053e520f911ac66c50aac5fdaf70a469
junghk0312@gmail.com <junghyunkim@gimjeonghyeon-ui-MacBookPro.local>
2024-02-12 12:00:49 +0900

    using set associative container

---
+++ Set/set.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<set>
+#include<string>
+
+
+template <typename T>
+void print_set(std::set<T>& inset){
+    for(const auto& elem: inset){
+        std::cout << elem << std::endl;
+    }
+}
+
+class Todo {
+    int priority;
+    std::string job_desc;
+    friend std::ostream & operator<< (std::ostream & o, const Todo& in);
+    
+    public: 
+        Todo(int priority, std::string job_desc)
+            : priority(priority), job_desc(job_desc) {}
+        bool operator<(const Todo& in) const {
+            if(priority == in.priority){
+                return job_desc < in.job_desc;
+            }
+            return (priority > in.priority);
+
+        }
+
+
+};
+
+std::ostream & operator<< (std::ostream & o, const Todo& in){
+    o << "priority:" << in.priority << std::endl;
+    o << "job desc:" << in.job_desc << std::endl;
+    o << std::endl;
+
+    return o;
+}
+
+int main() {
+    std::set<Todo> todos;
+    todos.insert(Todo(1,"abc"));
+    todos.insert(Todo(2,"abc"));
+    todos.insert(Todo(1,"bc"));
+    todos.insert(Todo(1,"efg"));
+    todos.insert(Todo(3,"cde"));
+    todos.insert(Todo(3,"bc"));
+
+    print_set(todos);
+}
