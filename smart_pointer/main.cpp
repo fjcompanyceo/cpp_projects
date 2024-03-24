4bbdcbfedb21f1d73c038edcf23c655f3dd26c6d
junghk0312@gmail.com <junghyunkim@gimjeonghyeon-ui-MacBookPro.local>
2024-03-24 22:59:34 +0900

    unique pointer 활용

--- smart_pointer/main.cpp
+++ smart_pointer/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<memory>
+#include<vector>
 
 class A {
     int * data;
@@ -20,7 +21,31 @@ class A {
     void anot() {
         std::cout << "ptr로 받아서 출력" << std::endl;
     }
+    void set_value(int in1) {
+        for(int i=0;i<=99;i++){
+            data[i] = in1;
+        }
+    }
+    void show_value() {
+        for(int i=0;i<=99;i++){
+            std::cout << data[i];
+        }
+        std::cout << std::endl;
+    }
+    
 
+};
+
+class Foo {
+    int a, b;
+
+    public:
+    Foo(int in1, int in2) : a(in1), b(in2) { std::cout << "Foo 클래스 생성자 호출" << std::endl;}
+    ~Foo() { std::cout << "소멸자 호출!" << std::endl;}
+
+    void print_val() {
+        std::cout << "a: " << a <<", b: " << b << std::endl;
+    }
 };
 
 void do_something() {
@@ -48,6 +73,26 @@ int main () {
     std::unique_ptr<A> pn(new A());
     //unique_ptr 사용할 때 .get() 으로 주소값을 넘겨야 한다. 
     do_with_ptr(pn.get());
+
+    auto Foo_ptr = std::make_unique<Foo> (3,5);
+    Foo_ptr->print_val();
+
+    std::vector<std::unique_ptr<A>> vec;
+    vec.emplace_back(new A());
+    vec.emplace_back(new A());
+    vec.emplace_back(new A());
+    vec[0]->set_value(0);
+    vec[1]->set_value(1);
+    vec[2]->set_value(2);
+    vec[0]->show_value();
+    vec[1]->show_value();
+    vec[2]->show_value();
+    //unique_ptr 로 이루어진 vector에 새로운 unique_ptr을 add 할 때 push back을 사용하고 싶다면?
+    std::unique_ptr<A> addA(new A());
+    vec.push_back(std::move(addA)); 
+    vec[3]->set_value(3);
+    vec[3]->show_value();
+
     return 0;
 }
 
