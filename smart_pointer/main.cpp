90bd63cfe117f4c4b53509733f40d93b879b5fd2
junghk0312@gmail.com <junghyunkim@gimjeonghyeon-ui-MacBookPro.local>
2024-03-24 22:22:24 +0900

    unique ptr 주소값을 넘기는 방법

--- smart_pointer/main.cpp
+++ smart_pointer/main.cpp
@@ -17,6 +17,9 @@ class A {
     void some() {
         std::cout << "일반포인터와 동일하게 사용가능" << std::endl;
     }
+    void anot() {
+        std::cout << "ptr로 받아서 출력" << std::endl;
+    }
 
 };
 
@@ -35,8 +38,16 @@ void do_something() {
     delete temp;
 }
 
+void do_with_ptr(A * in_ptr) {
+    in_ptr->anot();
+    return;
+}
+
 int main () {
     do_something();
+    std::unique_ptr<A> pn(new A());
+    //unique_ptr 사용할 때 .get() 으로 주소값을 넘겨야 한다. 
+    do_with_ptr(pn.get());
     return 0;
 }
 
